c6b8919e7caa61769c8ac09670ac43375634615c
Martín Medina <tinnmedina@gmail.com>
2017-11-20 18:20:12 -0300

    API method response is aware of REMASC payment cases depending on blocknumber, maturity, etc.

--- server.js
+++ server.js
@@ -21,9 +21,40 @@ router.route('/feepayment/block/:number([0-9]+)/:hash(0[xX]{1}[a-fA-F0-9]{64}$)'
 
     .get(async function(req, res) {
     	try {
-    		const paymentFees = await miningRepository.readFeePayment(req.params.number, req.params.hash);
+    		const blockNumber = req.params.number;
+    		const blockHash = req.params.hash;
+    		const callResult = {};
 
-	    	res.json(paymentFees);
+    		if(blockNumber < config.api.remasc.maturity + config.api.remasc.syntheticSpan) {
+
+    			callResult.message = "Block number " + blockNumber + " hasn't reach maturity + synthetic span yet.";
+    			callResult.value = null;
+
+    			return res.json(callResult);
+    		}
+
+ 			// WIP: Hardcoded value must be changed for value retrieved from DB
+    		const lastDb = 2000;
+    		if(lastDb < blockNumber && blockNumber < lastDb + config.api.remasc.maturity) {
+    			callResult.message = "Block number " + blockNumber + " hasn't reach maturity yet.";
+    			callResult.value = null;
+
+    			return res.json(callResult);
+    		}
+
+    		if(blockNumber > lastDb + config.api.remasc.maturity) {
+    			callResult.message = "Block number " + blockNumber + " hasn't been mined yet.";
+    			callResult.value = null;
+
+    			return res.json(callResult);
+    		}
+
+    		// Block number is in a valid range
+    		const paymentFees = await miningRepository.readFeePayment(blockNumber, blockHash);
+			callResult.message = "Payment fees found for block number " + blockNumber;
+			callResult.value = paymentFees;
+
+	    	return res.json(callResult);
     	} catch (err) {
     		logger.error(err);
     		res.status(500).send('Something broke!');
